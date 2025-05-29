b86b7d8ac05ede4f77166d5350c8acda46370c3d
dinaneq <dinanda@fitrianto.biz.id>
2025-05-29 21:50:05 +0700

    Update constants.js

--- api/constants.js
+++ api/constants.js
@@ -1,19 +1,11 @@
-export const startMessage = `👋 Hello there, UserName !
+export const startMessage = `👋 Halo kawan, UserName !
 
-Welcome to the *Auto Emoji Reaction Bot 🎉*, ready to sprinkle your conversations with a little extra happiness!
+Selamat datang di *Auto Emoji Reaction Bot 🎉*, bot yang akan membuat obrolanmu semakin berwarna dengan sedikit kebahagiaan!
 
-💁‍♂️ Here's how I spice up your chats:
+Daftar emoji: /reactions
+Donasi: /donate
 
-*✨ DM Magic*: Message me and receive a surprise emoji in return. Expect the unexpected and enjoy the fun!
-*🏖 Group & Channel*: Add me to your groups or channels, and I'll keep the vibe positive by reacting to messages with engaging emojis.
-
-✍️ To view the emojis I can use, simply type /reactions. To create your own auto reaction bot, head over to: [Auto-Reaction-Bot on GitHub](https://github.com/Malith-Rukshan/Auto-Reaction-Bot).
-
-Let's elevate our conversations with more energy and color! 🚀
-
-💖 Keep us running! Support the bot: /donate`
-
-export const donateMessage = `🙏 Support Auto Reaction Bot ✨ and help us stay online and continue to improve! ✨ Your donations keep our services live and enable us to bring you new features and enhancements. Every star makes a difference! Thank you! 🌟🚀`
+export const donateMessage = `🙏 Untuk ngasih donat bisa pakai QRIS di @BeriDukungan`
 
 export const htmlContent = `
 <!DOCTYPE html>
